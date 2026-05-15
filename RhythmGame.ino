d3c156b7fffdfa9f38b3618a1e66315da29da6e4
Starlight-dev-gen <turkevinagyhunor@gmail.com>
2026-05-15 19:18:14 +0200

    Add files via upload

--- RhythmGame.ino
+++ RhythmGame.ino
@@ -5,8 +5,11 @@ int lPressed = 0;
 const int rButton = 3;
 int rPressed = 0;
 bool gameRunning = false;
+bool selectionMode = false;
 String lStrip = "";
 String rStrip = "";
+int lNotes[] = {4, 8, 12, 16, 20, 24, 28, 32, 34, 36, 42, 44, 50, 52, 58, 60};
+int rNotes[] = {8, 16, 24, 32, 38, 40, 46, 48, 51, 54, 56, 59, 62, 64};
 
 void setup() {
   lcd.begin(16, 2);
@@ -22,13 +25,35 @@ void loop() {
   lPressed = digitalRead(lButton);
   rPressed = digitalRead(rButton);
 
-  if (!gameRunning){
+  if (!gameRunning && !selectionMode){
     if (lPressed == HIGH && rPressed == HIGH){
-      gameRunning = true;
+      selectionMode = true;
+      delay(15);
+    }
+  }
+  else if(!gameRunning && selectionMode){
+    if (lPressed == HIGH){
+      lcd.setCursor(0, 0);
+      lcd.print(">Normal");
+      lcd.setCursor(0, 1);
+      lcd.print(" Mirrored");
+      delay(15);
+    }
+    else if(rPressed == HIGH){
+      lcd.setCursor(0, 0);
+      lcd.print(" Normal");
+      lcd.setCursor(0, 1);
+      lcd.print(">Mirrored");
+    }
+    else{
+      lcd.setCursor(0, 0);
+      lcd.print(" Normal");
+      lcd.setCursor(0, 1);
+      lcd.print(" Mirrored");
     }
   }
   else{
-    lcd.setCursor(0, 0)
+    lcd.setCursor(0, 0);
     lcd.print(lStrip);
     lcd.setCursor(0, 1);
     lcd.print(rStrip);
